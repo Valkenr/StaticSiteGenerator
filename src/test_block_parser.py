c6a4a9890176c143fcdf4c1a31260593ad67ca1d
Valkenr <valkenr@valkenr.com>
2024-07-11 23:46:22 -0700

    extra block splitter test

--- src/test_block_parser.py
+++ src/test_block_parser.py
@@ -35,6 +35,32 @@ class TestInlineParser(unittest.TestCase):
             ],
         )
 
+    def test_2(self):
+        text = """
+# This is a heading
+
+
+
+
+
+
+
+This is a paragraph of text. It has some **bold** and *italic* words inside of it.
+
+
+* This is the first list item in a list block
+* This is a list item
+* This is another list item
+"""
+        self.assertEqual(
+            markdown_to_blocks(text),
+            [
+                '# This is a heading',
+                'This is a paragraph of text. It has some **bold** and *italic* words inside of it.',
+                '* This is the first list item in a list block\n* This is a list item\n* This is another list item'
+            ],
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
